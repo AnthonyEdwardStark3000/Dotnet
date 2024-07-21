b031ba4812811f726f64ddcd08ead933ea6ba678
MrAnthonyEdwardStark <sureshbabu.innowave@gmail.com>
2024-07-21 19:50:05 +0530

    Model binding reading param as argument in the function - HomeController.cs

--- BookStore/Controllers/HomeController.cs
+++ BookStore/Controllers/HomeController.cs
@@ -4,37 +4,24 @@ namespace IActionResultExample.Controllers
     public class HomeController:Controller{
         [Route("bookstore")]
         // url :/bookstore?bookid=5&isloggedin=true
-        public IActionResult Index(){
-            if(!Request.Query.ContainsKey("bookid")){
+        public IActionResult Index(int? bookid, bool? isLoggedIn){
+            if(bookid.HasValue==false){
                 return BadRequest("Book Id is not supplied!");
             }
-            if (string.IsNullOrEmpty(Convert.ToString(Request.Query["bookid"])))
-            {
-                Response.StatusCode = 400;
-                return Content("Book Id should contain a value");
+            if(bookid<=0){
+                return Content("Book Id cannot be less than or equal to zero!");
             }
-            int bookId = Convert.ToInt32(
-            ControllerContext.HttpContext.Request.Query["bookid"]);
-            if (bookId<=1 && bookId >= 100)
+            if (bookid<=1 && bookid >= 100)
             {
                 Response.StatusCode = 400;
                 return Content("Book Id should be between 1 and 100");
             }
             //?isLoggedIn is present in the query
-            if (!Convert.ToBoolean(Request.Query["isLoggedIn"])) {
+            if (isLoggedIn==false) {
                 Response.StatusCode = 401;
                 return Content("User must be authenticated to continue!");
             }
-            // return File("/sample.pdf","application/pdf");
-            
-            Console.WriteLine("check");
-            // return RedirectToAction("Check", "Store");
-            // return RedirectToAction("Books", "Store", new {id=3000}); // Redirected 302
-            // return RedirectToActionPermanent("Books","Store",new{id=2000}); // Redirect moved permanently 301
-            // Redirect to another local URL
-            // return new LocalRedirectResult($"/store/books/{bookId}"); // 302- Found or
-            // return LocalRedirect($"/store/books/{bookId}"); // 302 - Found
-            return LocalRedirectPermanent($"/store/books/{bookId}"); // 301 - Moved permanently.
+            return Content($"Book ID: {bookid}","text/plain");
     }
     }
 }
--- BookStore/obj/Debug/net8.0/BookStore.AssemblyInfo.cs
+++ BookStore/obj/Debug/net8.0/BookStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e8866c30a5914c73d24abf6ac538d517e11b06c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49d41781168e72ba3643351e9d80c47afef34689")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
