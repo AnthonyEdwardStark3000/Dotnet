e27599258ef42fbaa61fbfe07286a66fe6525474
MrAnthonyEdwardStark <sureshbabu.innowave@gmail.com>
2024-07-27 18:48:02 +0530

    Showing the dynamic field name and changing the display name for error message in Model State error

--- modelValidations/Models/Person.cs
+++ modelValidations/Models/Person.cs
@@ -2,8 +2,10 @@ using System.ComponentModel.DataAnnotations;
 
 namespace modelValidations.Models{
     public class Person{
-        [Required(ErrorMessage = "This field is an Required field and cannot be empty!")] // Attribute Required to make the PersonName field required one
+        [Required(ErrorMessage = "{0} field is an Required field and cannot be empty!")] // Attribute Required to make the PersonName field required one 
+        // {0} takes the first property name here it is PersonName
         //All the attributes are classes
+        [Display(Name="Person Name")] // Display Name to be shown in {0}
         public string? PersonName{get;set;}
         public string? Email{get;set;}
         public string? Phone{get;set;}
--- modelValidations/obj/Debug/net8.0/modelValidations.AssemblyInfo.cs
+++ modelValidations/obj/Debug/net8.0/modelValidations.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("modelValidations")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74ec0af05a86f2b35a0cafbb97cb7a8f28dc77d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+127f5a2aca5195495e567344bbb650944b12b05d")]
 [assembly: System.Reflection.AssemblyProductAttribute("modelValidations")]
 [assembly: System.Reflection.AssemblyTitleAttribute("modelValidations")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
