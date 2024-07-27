54b32785ca5e4df648922ca21240eabad5537707
MrAnthonyEdwardStark <sureshbabu.innowave@gmail.com>
2024-07-27 19:03:15 +0530

    All validations - 1, exectue after Model binding

--- modelValidations/Models/Person.cs
+++ modelValidations/Models/Person.cs
@@ -6,11 +6,13 @@ namespace modelValidations.Models{
         // {0} takes the first property name here it is PersonName
         //All the attributes are classes
         [Display(Name="Person Name")] // Display Name to be shown in {0}
+        [StringLength(40,MinimumLength =3,ErrorMessage ="{0} should contain between {2} - {1} characters")]
         public string? PersonName{get;set;}
         public string? Email{get;set;}
         public string? Phone{get;set;}
         public string? Password{get;set;}
         public string? ConfirmPassword{get;set;}
+        [Range(0,999.99, ErrorMessage ="{0} can contain numbers from the range {1} to {2}")]
         public double? Price{get;set;}
         public override string ToString(){
             return $"Person object - PersonName: {PersonName}\n Email: {Email}\n Phone: {Phone}\n Password: {Password}\n ConfirmPassword: {ConfirmPassword}\n Price: {Price}";
--- modelValidations/obj/Debug/net8.0/modelValidations.AssemblyInfo.cs
+++ modelValidations/obj/Debug/net8.0/modelValidations.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("modelValidations")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+127f5a2aca5195495e567344bbb650944b12b05d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e27599258ef42fbaa61fbfe07286a66fe6525474")]
 [assembly: System.Reflection.AssemblyProductAttribute("modelValidations")]
 [assembly: System.Reflection.AssemblyTitleAttribute("modelValidations")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
