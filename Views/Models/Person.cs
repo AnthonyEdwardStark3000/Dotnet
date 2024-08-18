d21e49ee9305e8349b1cde3392f8a94e4b0f2766
MrAnthonyEdwardStark <sureshbabu.innowave@gmail.com>
2024-08-18 17:19:55 +0530

    if, if-else and switch case in Razor pages

--- Views/Models/Person.cs
+++ Views/Models/Person.cs
@@ -1,6 +1,10 @@
 namespace Views.Models{
     public class Person{
         public string?Name{get;set;}
-        public DateTime DateOfBirth{get;set;}
+        public DateTime? DateOfBirth{get;set;}
+        public Gender PersonGender{get;set;}
     }
+        public enum Gender{
+            Male,Female,Other
+        }
 }
