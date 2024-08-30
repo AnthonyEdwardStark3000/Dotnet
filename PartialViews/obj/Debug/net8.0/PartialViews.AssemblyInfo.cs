e370889c736f19d3a6aee4a1bb675e18e51bbee5
MrAnthonyEdwardStark <sureshbabu.innowave@gmail.com>
2024-08-31 00:42:19 +0530

    Partial View - Created a Reusable view that can be invoked in any required place in the Project.

--- PartialViews/obj/Debug/net8.0/PartialViews.AssemblyInfo.cs
+++ PartialViews/obj/Debug/net8.0/PartialViews.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PartialViews")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02be907a901dba200a2d364ce6f6cb953620f5e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f30c4db84af00343a222ebd607f3c34b785baa4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("PartialViews")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PartialViews")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
