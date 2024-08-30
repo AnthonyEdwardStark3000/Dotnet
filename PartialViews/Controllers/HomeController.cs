f30c4db84af00343a222ebd607f3c34b785baa4e
MrAnthonyEdwardStark <sureshbabu.innowave@gmail.com>
2024-08-31 00:34:21 +0530

    Created Project with basic Views

---
+++ PartialViews/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+namespace PartialViews.Controllers{
+    public class HomeController:Controller{
+        [Route("/")]
+        public IActionResult Index(){
+            return View();
+        }
+
+        [Route("about")]
+        public IActionResult About(){
+            return View();
+        }
+    }
+}
