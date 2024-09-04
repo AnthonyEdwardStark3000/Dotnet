76a2774dbe49c7389f453ca121e0965b18d95caf
MrAnthonyEdwardStark <sureshbabu.innowave@gmail.com>
2024-09-04 23:13:38 +0530

    View Components

--- ViewComponent/ViewComponentsExample/GridViewComponent.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Mvc;
-
-namespace ViewComponentsExample.ViewComponents{
-    public class GridViewComponent: ViewComponent{
-        Task<IViewComponentResult> InvokeAsync(){ // Mandatory method for ViewComponent
-            return View(); // Invoke a partial view from Views/ Shared / Components / Grid / Default.cshtml  
-        }
-    }
-}
---
+++ ViewComponentsExample/ViewComponents/GridViewComponent.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ViewComponentsExample.ViewComponents{
+    public class GridViewComponent: ViewComponent{
+        public async Task<IViewComponentResult> InvokeAsync(){ // Mandatory method for ViewComponent
+        // The View here is not a regular view , it's an partial view.
+            return View(); // Invoke a partial view from Views/ Shared / Components / Grid / Default.cshtml  
+        }
+    }
+}
