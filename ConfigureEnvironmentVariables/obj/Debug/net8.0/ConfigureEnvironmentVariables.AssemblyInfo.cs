9a417f60d58e57d5e7cd69ad0435c4ee18ef8d1f
MrAnthonyEdwardStark <sureshbabu.innowave@gmail.com>
2024-09-27 00:16:34 +0530

    Accessing environmental variables in controller and Services and making use of environment tag helper in the view

--- ConfigureEnvironmentVariables/obj/Debug/net8.0/ConfigureEnvironmentVariables.AssemblyInfo.cs
+++ ConfigureEnvironmentVariables/obj/Debug/net8.0/ConfigureEnvironmentVariables.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConfigureEnvironmentVariables")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cf6e288536fe9384444339a028cc782f0e2f5e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7c4f8d3b1d00665c4c129ba9cfefea1888d0622")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConfigureEnvironmentVariables")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConfigureEnvironmentVariables")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
